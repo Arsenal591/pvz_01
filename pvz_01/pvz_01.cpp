ffc00ff9531c813ebacd4a58cfafda892fb7da47
Arsenal591 <arsenal591@126.com>
2016-08-28 18:38:13 +0800

    增加了播放音乐

--- pvz_01/pvz_01.cpp
+++ pvz_01/pvz_01.cpp
@@ -1,9 +1,17 @@
 #include "pvz_01.h"
 #include <qdialog>
+#include <qsound.h>
+#include <qurl.h>
+#include <qmediaplayer>
 
 MainWindow::MainWindow(QWidget* parent)
 {
 	_currentWidget = new WelcomeInterface;
 	_currentWidget->show();
-
+	//QSound music("../pvz-material/audio/2.75.mp3");
+	//music.play();
+	QMediaPlayer* player = new QMediaPlayer;
+	player->setMedia(QUrl::fromLocalFile("../pvz-material/audio/2.75.mp3"));
+	player->setVolume(100);
+	player->play();
 }
